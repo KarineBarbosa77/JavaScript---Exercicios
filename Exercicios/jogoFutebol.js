7bb06c4f0e940e88e5773aa1107435a4981aa4ff
KarineBarbosa77 <160532475+KarineBarbosa77@users.noreply.github.com>
2024-03-19 19:47:56 +0000

    exercicio

---
+++ Exercicios/jogoFutebol.js
@@ -0,0 +1,21 @@
+// Desenvolva um programa que recebe do usuário, o placar de um jogo de futebol 
+//(os gols de cada time) e informa se o resultado foi um empate, se a vitória foi do primeiro 
+//time ou do segundo time.
+
+
+//entrada de dados
+// = informar os dois times de futebol
+// = informar o placar do jogo
+
+
+//processamento
+// = realizar a validação através das estruturas condicionais
+// = verificar se os times ganharam, perderam ou empataram
+
+//saida
+// = informar o resultado do jogo de futebol
+let A = parseInt
+
+alert('Quanto gols teve o time ' + A)
+alert('Quantos gols teve o time ' + B)
+
