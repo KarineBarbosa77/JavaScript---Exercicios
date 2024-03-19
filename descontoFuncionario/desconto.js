6cb0d3c053862e581f364c8c4d08dbd9718f197c
KarineBarbosa77 <160532475+KarineBarbosa77@users.noreply.github.com>
2024-03-19 19:28:26 +0000

    exercicio

--- descontoFuncionario/desconto.js
+++ descontoFuncionario/desconto.js
@@ -5,19 +5,21 @@ let escolha = parseInt(opcao)
 switch (escolha) {
     case 1:
         alert('Funcionario\nDesconto de 10%')
-        prompt('Digite o total da compra: '+ valorTotal)
-        let valorDesconto = parseFloat (10/100)*valorTotal
-        alert('Valor do desconto: R$' + valorDesconto)
+        let valorTotalFuncionario = parseFloat(prompt('Digite o total da compra: '));
+        let valorDesconto1 = 0.10 * valorTotalFuncionario; // 10% de desconto
+        alert('Valor do desconto R$' + valorDesconto1.toFixed(2));
         break
     case 2:
         alert('Cliente\nSem desconto')
-        prompt('Digite o total da compra: ' + valorDesconto)
+        let valorTotalCliente = parseFloat(prompt('Digite o total da compra: '));
+        alert('Valor do desconto R$' + valorTotalCliente.toFixed(2));
         
         break
     case 3:
         alert('Vip\nDesconto de 5%')
-        prompt('Digite o total da compra: ' + valorDesconto)
-        alert('Total: ')
+        let valorTotalVip = parseFloat(prompt('Digite o total da compra: '));
+        let valorDesconto2 = 0.05 * valorTotalVip; // 10% de desconto
+        alert('Valor do desconto R$' + valorDesconto2.toFixed(2));
     default:
         alert('Opção invalida')
 
