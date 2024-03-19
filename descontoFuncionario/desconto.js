42318ae837e8aa16d5d47bf5bba2ed09ab9e5aa8
KarineBarbosa77 <160532475+KarineBarbosa77@users.noreply.github.com>
2024-03-19 18:52:57 +0000

    exercicios

--- descontoFuncionario/desconto.js
+++ descontoFuncionario/desconto.js
@@ -1,19 +1,29 @@
 let opcao = prompt('Escolha a opção\n 1- Funcionario\n 2- Cliente\n 3- Vip')
 
+let f = (10/100)*valorTotal
+let c = valorTotal
+let v = (5/100)*valorTotal
 let escolha = parseInt(opcao)
+
 
 switch (escolha) {
     case 1:
         alert('Funcionario\nDesconto de 10%')
+        prompt('Digite o total da compra: ' + valorTotal)
+        alert('Total: ' + f)
         
         break
     case 2:
         alert('Cliente\nSem desconto')
+        prompt('Digite o total da compra: ' + valorTotal)
+        alert('Total: ' + c)
         break
     case 3:
         alert('Vip\nDesconto de 5%')
-       
+        prompt('Digite o total da compra: ' + valorTotal)
+        alert('Total: ' + v)
     default:
         alert('Opção invalida')
 
 }
+
