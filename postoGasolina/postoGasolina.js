2930ac1f63cb1682b475481a43ea3f1885ef8fda
KarineBarbosa77 <160532475+KarineBarbosa77@users.noreply.github.com>
2024-03-20 19:24:58 +0000

    exercicio

--- postoGasolina/postoGasolina.js
+++ postoGasolina/postoGasolina.js
@@ -1,16 +1,25 @@
 let opcao = prompt ('Escolha\n 1- Gasolina\n 2- Alcool')
-let gas = "Gasolina"
-let alc = "Alcool"
 
 let escolha = parseInt(opcao)
 
 switch (escolha) {
     case 1:
         alert('Opcao escolhida: Gasolina')
+        let gas = parseFloat(prompt('Digite o total de litros: '))
+        if(gas <= 20){
+            let total = gas * 5.3
+            alert('Desconto de 4%\n')
+            
+        }else{
+            alert('')
+        }
         break
     case 2:
         alert('Opcao escolhida: Álcool')
+        let alc = parseFloat(prompt('Digite o total de litros: '))
         break
     default:
         alert('Opcao Inválida')
 }
+//litro gasolina = 5,30 
+//
